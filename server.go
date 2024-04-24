e3dc8cd0318b43bd0ada9a3acbf959b345f7fc28
Benjababe <bengohzy@gmail.com>
2024-04-24 16:20:11 +0800

    Update pkg consul registry (#1)
    * feat: use consul host for registering
    
    * feat: include linter for pkg
    
    * refactor: linter fixes
    
    * refactor: linter fixes

--- server.go
+++ server.go
@@ -32,19 +32,19 @@ func Boot() *server.Hertz {
 	// set cors
 	var corsc = con.Get("corscfg")
 
-	//转换
+	// 转换
 	cor, ok := corsc.(cors.Config)
 	if ok {
 		// 跨域等等头部设置在配置文件 /config/cors.go中
 		h.Use(cors.New(cor))
 	}
 
-	//write to log
+	// Write to log
 	facades.Log().Info(strings.ToUpper(facades.Config().GetString("APP_MODULE")) + " Server Started:")
 	return h
 }
 
 // svrconn -- just used self
-func svrconn(c context.Context, con network.Conn) context.Context {
+func svrconn(c context.Context, _ network.Conn) context.Context {
 	return c
 }
