f938971838116a06e20a4a4c327a58d9429ea5bf
benjababe <bengohzy@gmail.com>
2024-04-24 16:12:23 +0800

    feat: use consul host for registering

--- consul.go
+++ consul.go
@@ -14,11 +14,10 @@ import (
 )
 
 var (
-	wg      sync.WaitGroup
-	localIP = "127.0.0.1"
-	h       *server.Hertz
-	info    *registry.Info
-	r       registry.Registry
+	wg   sync.WaitGroup
+	h    *server.Hertz
+	info *registry.Info
+	r    registry.Registry
 )
 
 // Register -- register service to consul
@@ -26,25 +25,24 @@ func Register() *server.Hertz {
 	cfg := facades.Config()
 
 	//set consul server ip
-	consulHost := cfg.GetString("host", "127.0.0.1")
-	consulPort := cfg.GetString("port", "8500")
+	consulHost := cfg.GetString("CONSUL_HOST", "127.0.0.1")
+	consulPort := cfg.GetString("CONSUL_PORT", "8500")
 
 	consulCfg := consulapi.DefaultConfig()
 	consulCfg.Address = net.JoinHostPort(consulHost, consulPort)
 	fmt.Println(consulCfg.Address)
-	conclient, err := consulapi.NewClient(consulCfg)
+	consulClient, err := consulapi.NewClient(consulCfg)
 	if err != nil {
 		facades.Log().Panic(err)
 		return nil
 	}
 	wg.Add(2)
-
 	defer wg.Done()
 
-	//set localIP for host
-	localIP = Myip()
-	addr := net.JoinHostPort(localIP, cfg.GetString("APP_PORT", "8000"))
-	r = consul.NewConsulRegister(conclient)
+	//set localIP for serviceHost
+	serviceHost := cfg.GetString("APP_URL", "http://localhost")
+	addr := net.JoinHostPort(serviceHost, cfg.GetString("APP_PORT", "8000"))
+	r = consul.NewConsulRegister(consulClient)
 	info = &registry.Info{
 		ServiceName: cfg.GetString("APP_MODULE", "Demo"),
 		Addr:        utils.NewNetAddr("tcp", addr),
@@ -59,37 +57,4 @@ func Register() *server.Hertz {
 	)
 	return h
 
-}
-
-// Myip  -- get myip
-func Myip() string {
-
-	// 获取本机所有网络接口的信息
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		fmt.Println("获取网络接口信息失败:", err)
-		return "0.0.0.0"
-	}
-
-	// 遍历每个网络接口，查找非回环接口并获取其 IPv4 地址
-	for _, iface := range interfaces {
-		// 排除回环接口和无效接口
-		if iface.Flags&net.FlagLoopback == 0 && iface.Flags&net.FlagUp != 0 {
-			// 获取接口的地址列表
-			addrs, err := iface.Addrs()
-			if err != nil {
-				fmt.Println("获取接口地址失败:", err)
-				continue
-			}
-			// 遍历地址列表，查找 IPv4 地址
-			for _, addr := range addrs {
-				ipNet, ok := addr.(*net.IPNet)
-				if ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
-					//fmt.Println("本机 IPv4 地址:", ipNet.IP)
-					return ipNet.IP.String()
-				}
-			}
-		}
-	}
-	return "0.0.0.0"
 }
