00df9133ff4c45a8614cf49339038ab059213ad3
benjababe <bengohzy@gmail.com>
2024-05-09 15:35:25 +0800

    feat: Encode/decode ciphertext with base64

--- crypto.go
+++ crypto.go
@@ -4,31 +4,40 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	b64 "encoding/base64"
 	"io"
 )
 
 const nonceSize = 12
 
-func Encrypt(key, plainText []byte) ([]byte, error) {
+func Encrypt(key, plainText []byte) (string, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	aesGCM, err := cipher.NewGCM(block)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	nonce := make([]byte, nonceSize)
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	cipherText := aesGCM.Seal(nil, nonce, plainText, nil)
+
+	b64CipherText := b64.URLEncoding.EncodeToString(cipherText)
+	return b64CipherText, nil
+}
+
+func Decrypt(key []byte, b64CipherText string) ([]byte, error) {
+	cipherText, err := b64.URLEncoding.DecodeString(b64CipherText)
+	if err != nil {
 		return nil, err
 	}
 
-	return aesGCM.Seal(nil, nonce, plainText, nil), nil
-}
-
-func Decrypt(key, cipherText []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
