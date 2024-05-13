30bb047dc009082cb0cd657bed857e6c91f3a9cb
benjababe <bengohzy@gmail.com>
2024-05-13 09:57:34 +0800

    feat: add user id retrieval

--- user.go
+++ user.go
@@ -1,17 +1,44 @@
 package pkg
 
 import (
+	"errors"
+	"strconv"
+	"strings"
+
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/goravel/framework/facades"
+	"github.com/goravel/framework/http"
 )
 
-func GetUserID(c *app.RequestContext) (string, error) {
-	userIDEnc := string(c.GetHeader(UserIDHeaderKey))
-	userIDBytes, err := Decrypt([]byte(facades.Config().GetString("WATT_AES_SHARED_KEY", "")), userIDEnc)
+func GetJWTFromHeader(c *app.RequestContext) string {
+	authHeader := string(c.GetHeader("Authorization"))
+	spl := strings.Split(authHeader, " ")
+	if spl[0] != "Bearer" {
+		return ""
+	}
+	return spl[1]
+}
+
+func GetUserIdFromJWT(token string) (int32, error) {
+	decodedPayload, err := facades.Auth().Parse(http.Background(), token)
 	if err != nil {
-		return "", err
+		return 0, errors.New("failed to decode JWT")
 	}
 
-	userID := string(userIDBytes)
+	userIDStr := decodedPayload.Key
+	userID, err := strconv.ParseInt(userIDStr, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return int32(userID), nil
+}
+
+func GetUserId(c *app.RequestContext) (int32, error) {
+	jwt := GetJWTFromHeader(c)
+	userID, err := GetUserIdFromJWT(jwt)
+	if err != nil {
+		return 0, err
+	}
 	return userID, nil
 }
