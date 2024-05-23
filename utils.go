534dcb1321a7c8a669bef94dc5f9f6a72ffe3713
benjababe <bengohzy@gmail.com>
2024-05-23 15:12:04 +0800

    fix: Add year to stringtotime

--- utils.go
+++ utils.go
@@ -1,19 +1,21 @@
 package pkg
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+const timeDtLayout = "2010-01-01 %s"
 const timeLayout = "15:04"
-const timeDtLayout = "2000-01-01 15:04"
 const dateLayout = "02-01-2006"
 
 // Converts 24 hour time in the format "HH:mm" to a time.Time object.
 // Set dt to true if the date is required like for MySQL.
 func StringToTime(timeStr string, dt bool) (time.Time, error) {
 	if dt {
-		return time.Parse(timeDtLayout, timeStr)
-	} else {
-		return time.Parse(timeLayout, timeStr)
+		timeStr = fmt.Sprintf(timeDtLayout, timeStr)
 	}
+	return time.Parse(timeLayout, timeStr)
 }
 
 // Converts a time.Time object to a 24 hour string in the format "HH:mm"
