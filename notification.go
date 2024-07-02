34864742603858f2946a4017d93411851b80cc85
benjababe <bengohzy@gmail.com>
2024-07-02 09:57:03 +0800

    feat: update notif functions

--- notification.go
+++ notification.go
@@ -8,9 +8,7 @@ import (
 	"github.com/goravel/framework/facades"
 )
 
-type PushNotificationBody struct {
-	Type      int32  `json:"type"`
-	Token     string `json:"string"`
+type PushTopicBody struct {
 	Topic     string `json:"topic"`
 	Title     string `json:"title"`
 	Body      string `json:"body"`
@@ -18,23 +16,63 @@ type PushNotificationBody struct {
 	SharedKey string `json:"shared_key"`
 }
 
+type PushTokensBody struct {
+	Tokens    []string `json:"tokens"`
+	Title     string   `json:"title"`
+	Body      string   `json:"body"`
+	ImageURL  string   `json:"image_url"`
+	SharedKey string   `json:"shared_key"`
+}
+
 type PushNotificationResponse struct {
 	StatusCode int32  `json:"status_code"`
 	Msg        string `json:"msg"`
 }
 
-func SendPushNotification(pushType int32, identifier, title, body, imageURL string) error {
+// Send push notifications to topic
+func SendPushNotificationTopic(pushType int32, identifier, title, body, imageURL string) error {
 	var resBody PushNotificationResponse
 
-	url := facades.Config().GetString("WATT_NOTIFICATION_PUSH_URL", "")
+	url := facades.Config().GetString("WATT_NOTIFICATION_PUSH_TOPIC_URL", "")
 	if url == "" {
-		return errors.New("WATT_NOTIFICATION_PUSH_URL is not set, unable to send push notification")
+		return errors.New("WATT_NOTIFICATION_PUSH_TOPIC_URL is not set, unable to send push notification")
 	}
 
-	reqBody := PushNotificationBody{
-		Type:      pushType,
-		Token:     identifier,
+	reqBody := PushTopicBody{
 		Topic:     identifier,
+		Title:     title,
+		Body:      body,
+		ImageURL:  imageURL,
+		SharedKey: facades.Config().GetString("WATT_NOTIFICATION_SHARED_KEY"),
+	}
+
+	client := resty.New()
+	res, err := client.R().
+		SetBody(reqBody).
+		SetResult(&resBody).
+		SetError(&resBody).
+		Post(url)
+	if err != nil {
+		return err
+	}
+	if res.IsError() {
+		return fmt.Errorf("error pushing notifications: %s", resBody.Msg)
+	}
+
+	return nil
+}
+
+// Send push notifications to a bunch of tokens
+func SendPushNotificationTokens(tokens []string, title, body, imageURL string) error {
+	var resBody PushNotificationResponse
+
+	url := facades.Config().GetString("WATT_NOTIFICATION_PUSH_TOKENS_URL", "")
+	if url == "" {
+		return errors.New("WATT_NOTIFICATION_PUSH_TOKENS_URL is not set, unable to send push notification ")
+	}
+
+	reqBody := PushTokensBody{
+		Tokens:    tokens,
 		Title:     title,
 		Body:      body,
 		ImageURL:  imageURL,
