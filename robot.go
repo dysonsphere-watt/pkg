1d9624bc500042c7d48f84c55134ac26e4ff8452
benjababe <bengohzy@gmail.com>
2024-08-01 15:12:34 +0800

    feat: include to robot cmd msg

--- robot.go
+++ robot.go
@@ -11,6 +11,11 @@ type QRCodeMessage struct {
 	QRCodeContent string `json:"qr_code_content"`
 }
 
+type ToRobotCommandMessage struct {
+	Command string `json:"command"`
+	DataB64 []byte `json:"data_b64"`
+}
+
 type ToRobotMQTTMessage struct {
 	TaskID  int32  `json:"task_id"`
 	Payload []byte `json:"payload_b64"`
